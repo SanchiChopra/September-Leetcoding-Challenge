b4ea1de6a5dad89c0b21aeaa0fb2d72aa028cdc3
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-09-08 22:11:25 +0530

    Sum of Root to Leaf Binary Numbers

---
+++ Day8.cpp
@@ -0,0 +1,53 @@
+/* QUESTION: Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+Example 1:
+
+
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* rt) {
+        if(! rt) return 0;
+        
+        int ans = 0;
+        stack<pair<TreeNode*,int>> st;
+        st.push({rt,0});
+        
+        while(! st.empty()) {
+            auto curp = st.top(); st.pop();
+            TreeNode* cur = curp.first;
+            int cur_val = curp.second;
+            
+            // Equivalent to left shift by 1 and then ORing by cur->val.
+            cur_val = cur_val*2 + cur->val;
+            
+            if(!cur->left && !cur->right)
+                ans += cur_val;
+            
+            if(cur->left) st.push({cur->left, cur_val});
+            if(cur->right) st.push({cur->right, cur_val});
+        }
+        
+        return ans;
+        
+    }
+};
